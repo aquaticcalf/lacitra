4f3705e29bed0d7ebd196acbee09b934caed7df2
sam <aquaticcalf@proton.me>
2026-06-04 01:42:47 +0530

    deinit s3 connection

--- s3/index.zig
+++ s3/index.zig
@@ -4,3 +4,7 @@ const S3Client = @import("z3").S3Client;
 pub fn initS3Connection(allocator: std.mem.Allocator, config: S3Client.Config) !S3Client {
     return try S3Client.init(allocator, config, .{});
 }
+
+pub fn deinitS3Connection(client: *S3Client) void {
+    client.deinit();
+}
